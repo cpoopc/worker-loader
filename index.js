0f7dff0b74ee2d400981a6dec773eebde9222f90
cpoopc <303727604@qq.com>
2016-08-28 22:02:25 +0800

    add url mode:require('cpoopc-webworker?url!script.js) return script's url

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * Created by cpoopc on 2016/8/27.
+ */
 var WebWorkerTemplatePlugin = require("webpack/lib/webworker/WebWorkerTemplatePlugin");
 var SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
 var path = require("path");
@@ -5,52 +8,57 @@ var path = require("path");
 var loaderUtils = require("loader-utils");
 module.exports = function() {};
 module.exports.pitch = function(request) {
-	if(!this.webpack) throw new Error("Only usable with webpack");
-	this.cacheable(false);
-	var callback = this.async();
-	var query = loaderUtils.parseQuery(this.query);
-	var filename = loaderUtils.interpolateName(this, query.name || "[hash].worker.js", {
-		context: query.context || this.options.context,
-		regExp: query.regExp
-	});
-	var outputOptions = {
-		filename: filename,
-		chunkFilename: "[id]." + filename,
-		namedChunkFilename: null
-	};
-	if(this.options && this.options.worker && this.options.worker.output) {
-		for(var name in this.options.worker.output) {
-			outputOptions[name] = this.options.worker.output[name];
-		}
-	}
-	var workerCompiler = this._compilation.createChildCompiler("worker", outputOptions);
-	workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
-	workerCompiler.apply(new SingleEntryPlugin(this.context, "!!" + request, "main"));
-	if(this.options && this.options.worker && this.options.worker.plugins) {
-		this.options.worker.plugins.forEach(function(plugin) {
-			workerCompiler.apply(plugin);
-		});
-	}
-	var subCache = "subcache " + __dirname + " " + request;
-	workerCompiler.plugin("compilation", function(compilation) {
-		if(compilation.cache) {
-			if(!compilation.cache[subCache])
-				compilation.cache[subCache] = {};
-			compilation.cache = compilation.cache[subCache];
-		}
-	});
-	workerCompiler.runAsChild(function(err, entries, compilation) {
-		if(err) return callback(err);
-		if (entries[0]) {
-			var workerFile = entries[0].files[0];
-			var constructor = "new Worker(__webpack_public_path__ + " + JSON.stringify(workerFile) + ")";
-			if(query.inline) {
-				constructor = "require(" + JSON.stringify("!!" + path.join(__dirname, "createInlineWorker.js")) + ")(" +
-					JSON.stringify(compilation.assets[workerFile].source()) + ", __webpack_public_path__ + " + JSON.stringify(workerFile) + ")";
-			}
-			return callback(null, "module.exports = function() {\n\treturn " + constructor + ";\n};");
-		} else {
-			return callback(null, null);
-		}
-	});
+    if(!this.webpack) throw new Error("Only usable with webpack");
+    this.cacheable(false);
+    var callback = this.async();
+    var query = loaderUtils.parseQuery(this.query);
+    var filename = loaderUtils.interpolateName(this, query.name || "[hash].worker.js", {
+        context: query.context || this.options.context,
+        regExp: query.regExp
+    });
+    var outputOptions = {
+        filename: filename,
+        chunkFilename: "[id]." + filename,
+        namedChunkFilename: null
+    };
+    if(this.options && this.options.worker && this.options.worker.output) {
+        for(var name in this.options.worker.output) {
+            outputOptions[name] = this.options.worker.output[name];
+        }
+    }
+    var workerCompiler = this._compilation.createChildCompiler("worker", outputOptions);
+    workerCompiler.apply(new WebWorkerTemplatePlugin(outputOptions));
+    workerCompiler.apply(new SingleEntryPlugin(this.context, "!!" + request, "main"));
+    if(this.options && this.options.worker && this.options.worker.plugins) {
+        this.options.worker.plugins.forEach(function(plugin) {
+            workerCompiler.apply(plugin);
+        });
+    }
+    var subCache = "subcache " + __dirname + " " + request;
+    workerCompiler.plugin("compilation", function(compilation) {
+        if(compilation.cache) {
+            if(!compilation.cache[subCache])
+                compilation.cache[subCache] = {};
+            compilation.cache = compilation.cache[subCache];
+        }
+    });
+    //fs.writeFile('cplog.txt', JSON.stringify(request) + '\n query:' + JSON.stringify(query) + '\n option:' + JSON.stringify(this.options), null, 2)
+    workerCompiler.runAsChild(function(err, entries, compilation) {
+        if(err) return callback(err);
+        if (entries[0]) {
+            var workerFile = entries[0].files[0];
+            if(query.url) {
+                // 仅返回url
+                return callback(null, "var url = __webpack_public_path__ + " + JSON.stringify(workerFile) + ";\n\tmodule.exports = url")
+            }
+            var constructor = "new Worker(__webpack_public_path__ + " + JSON.stringify(workerFile) + ")";
+            if(query.inline) {
+                constructor = "require(" + JSON.stringify("!!" + path.join(__dirname, "createInlineWorker.js")) + ")(" +
+                    JSON.stringify(compilation.assets[workerFile].source()) + ", __webpack_public_path__ + " + JSON.stringify(workerFile) + ")";
+            }
+            return callback(null, "module.exports = function() {\n\treturn " + constructor + ";\n};");
+        } else {
+            return callback(null, null);
+        }
+    });
 };
